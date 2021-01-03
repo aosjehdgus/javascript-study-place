8da4f152a64bbe9770647b136e9d4dc4256f0314
kdhsea <kdhsea@gmail.com>
2021-01-03 23:15:25 +0900

    practice_javascript-object_inheritance+create

---
+++ js_study/JS_object_oriented_programming/inheritance-2.js
@@ -0,0 +1,10 @@
+var superObj = {superVal:'super'}
+var subObj = Object.create(superObj);
+
+subObj.subVal = 'sub';
+console.log('subObj.subVal =>', subObj.subVal);
+console.log('subObj.superVal =>', subObj.superVal);
+
+subObj.superVal = 'sub';
+console.log('superObj.superVal =>', superObj.superVal);
+
