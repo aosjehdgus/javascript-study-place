898e3e69501f782296606dc58ced37a85d01e8ca
kdhsea <kdhsea@gmail.com>
2021-01-06 20:51:27 +0900

    react-create-component

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,16 +1,62 @@
+import { render } from '@testing-library/react';
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+class Head extends Component{
+  render(){
+    return(
+
+      <header>
+        <h1> WEB </h1>
+        World Wide Web!!
+      </header>
+
+    );
+  }
+}
+
+class TOC extends Component{
+  render(){
+    return(
+      <nav>
+        <ul>
+            <li><a href="1.html">HTML</a></li>
+            <li><a href="2.html">CSS</a></li>
+            <li><a href="3.html">JavaScript</a></li>
+
+        </ul>
+      </nav>
+    );
+
+  }
+}
+
+class Contents extends Component{
+  render(){
+    return(
+      <article>
+      <h2> HTML </h2>
+
+      HTML is HyperText Markup Language.
+      </article>
+    );
+  }
+}
+
 
 class App extends Component{
   render(){
     return (
       <div className="App">
-
+        <Head> </Head>
+        <TOC> </TOC>
+        <Contents></Contents>
       </div>
     );
   }
 }
+
+
 export default App;
 
 // function App() {
