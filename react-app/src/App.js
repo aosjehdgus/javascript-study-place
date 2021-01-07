ee4382b52f5edc66c6ee6ffcb0dd4c65838c1267
kdhsea <kdhsea@gmail.com>
2021-01-07 14:48:42 +0900

    react-using props

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,61 +2,85 @@ import { render } from '@testing-library/react';
 import React, { Component } from 'react';
 import './App.css';
 
-class Head extends Component{
+// class Head extends Component{
+//   render(){
+//     return(
+
+//       <header>
+//         <h1> {this.props.title} </h1>
+//         {this.props.sub}
+//       </header>
+
+//     );
+//   }
+// }
+
+// class TOC extends Component{
+//   render(){
+//     return(
+//       <nav>
+//         <ul>
+//             <li><a href="1.html">HTML</a></li>
+//             <li><a href="2.html">CSS</a></li>
+//             <li><a href="3.html">JS</a></li>
+
+//         </ul>
+//       </nav>
+//     );
+
+//   }
+// }
+
+// class Contents extends Component{
+//   render(){
+//     return(
+//       <article>
+//       <h2> {this.props.title} </h2>
+
+//       {this.props.desc}
+//       </article>
+//     );
+//   }
+// }
+
+
+// class App extends Component{
+//   render(){
+//     return (
+//       <div className="App">
+//         <Head title="WEB" sub = "world wide web!"> </Head>
+//         <Head title="React" sub = "For UI"> </Head>
+//         <TOC> </TOC>
+//         <Contents title = "HTML" desc = "HTML is HyperText Markup Language."></Contents>
+//         <Contents title = "CSS" desc = "CSS is Cascading Style Sheets."></Contents>
+//         <Contents title = "JS" desc = "JS is Javascripts."></Contents>
+//         <Example_props></Example_props>
+//       </div>
+//     );
+//   }
+// }
+
+class Example_props extends Component{
   render(){
     return(
-
-      <header>
-        <h1> WEB </h1>
-        World Wide Web!!
-      </header>
-
-    );
-  }
-}
-
-class TOC extends Component{
-  render(){
-    return(
-      <nav>
-        <ul>
-            <li><a href="1.html">HTML</a></li>
-            <li><a href="2.html">CSS</a></li>
-            <li><a href="3.html">JavaScript</a></li>
-
-        </ul>
-      </nav>
-    );
-
-  }
-}
-
-class Contents extends Component{
-  render(){
-    return(
-      <article>
-      <h2> HTML </h2>
-
-      HTML is HyperText Markup Language.
-      </article>
-    );
-  }
-}
-
-
-class App extends Component{
-  render(){
-    return (
-      <div className="App">
-        <Head> </Head>
-        <TOC> </TOC>
-        <Contents></Contents>
+      <div>
+         <h1> {this.props.title}</h1>
+          {this.props.desc}
       </div>
     );
   }
 }
 
-
+class App extends Component{
+  render(){
+    return(
+    <div className = "App">
+      <Example_props title = "props" desc = "props stands for properties."></Example_props>
+      <Example_props title = "react" desc = "UI interface"></Example_props>
+    </div>
+    );
+  }
+}
 export default App;
 
 // function App() {
