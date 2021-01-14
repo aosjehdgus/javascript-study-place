4997687552c96f0b875d6ffc4e44bd5967847282
kdhsea <kdhsea@gmail.com>
2021-01-14 17:45:15 +0900

    react - add tittle css

--- react-app/src/components/Head.js
+++ react-app/src/components/Head.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import './title.css'
 
 class Head extends Component{
     render(){
       return(
   
         <header>
-          <h1> {this.props.title} </h1>
+
+          <h1 className = "main_title"> {this.props.title} </h1>
+          <h1 className = "sub_title"> {this.props.sub} </h1>
     
         </header>
   
